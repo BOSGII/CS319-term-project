30771e75e71dd56b4c2e087b31f929ed00040f9f
inceisciberkay <110044051+inceisciberkay@users.noreply.github.com>
2023-05-05 11:24:38 +0300

    Feature f3 (#9)

---
+++ frontend/src/contexts/UserContext.js
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  // Initialize state variables for the user's ID and role
+  const [user, setUser] = useState({ id: null, role: null });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
