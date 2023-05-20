6f775b9128ece175d59caee4d0b2d9e8bbd96be5
safa55 <eren.kuday@ug.bilkent.edu.tr>
2023-05-20 17:16:53 +0300

    Import Service

--- backend/src/main/java/com/bosgii/internshipmanagement/services/ImportService.java
+++ backend/src/main/java/com/bosgii/internshipmanagement/services/ImportService.java
@@ -4,32 +4,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.hibernate.metamodel.model.domain.internal.ArrayTupleType;
+//note: 2 versions for 2 different excel types xls and xlsx, one uses "poi.ss" other "poi.sl"
 
+import com.bosgii.internshipmanagement.entities.Company;
 import com.bosgii.internshipmanagement.entities.Internship;
+import com.bosgii.internshipmanagement.entities.Student;
+import com.bosgii.internshipmanagement.entities.Supervisor;
 import com.bosgii.internshipmanagement.enums.InternshipType;
-import com.bosgii.internshipmanagement.requests.AddInternshipRequest;
+import com.bosgii.internshipmanagement.repos.CompanyRepository;
+import com.bosgii.internshipmanagement.repos.InternshipRepository;
+import com.bosgii.internshipmanagement.repos.StudentRepository;
+import com.bosgii.internshipmanagement.repos.SupervisorRepository;
+//import com.bosgii.internshipmanagement.requests.AddInternshipRequest;
 
 @Service
 public class ImportService {
-    private final InternshipService internshipService;
+    //private final InternshipService internshipService;
+    private final StudentRepository studentRepository;
+    private final InternshipRepository internshipRepository;
+    private final CompanyRepository companyRepository;
+    private final SupervisorRepository supervisorRepository;
 
-    public ImportService(InternshipService internshipService) {
-        this.internshipService = internshipService;
+    
+    public ImportService(StudentRepository studentRepository, InternshipRepository internshipRepository,
+            CompanyRepository companyRepository, SupervisorRepository supervisorRepository) {
+        this.studentRepository = studentRepository;
+        this.internshipRepository = internshipRepository;
+        this.companyRepository = companyRepository;
+        this.supervisorRepository = supervisorRepository;
     }
 
     public List<Internship> importInternshipsFromExcelFile(MultipartFile file) {
         List<Internship> list = new ArrayList<Internship>();
         try (Workbook workbook = WorkbookFactory.create(file.getInputStream())) {
-            Sheet sheet = (Sheet) workbook.getSheetAt(0);
+            Sheet sheet = workbook.getSheetAt(0);
 
             for (Row row : sheet) {
                 ArrayList<Cell> rowInfo = new ArrayList<Cell>();
@@ -58,8 +75,67 @@ public class ImportService {
                 String supervisorGraduationDepartment = rowInfo.get(12).getStringCellValue();
                 String supervisorUniversity = rowInfo.get(13).getStringCellValue();
                 
-                AddInternshipRequest req = new AddInternshipRequest(type, startDate, endDate, studentId, studentFullName, studentMail, studentDepartment, companyName, companyEmail, supervisorName, supervisorMail, supervisorGraduationYear, supervisorGraduationDepartment, supervisorUniversity)
-                list.add(internshipService.addInternship(req));
+                //AddInternshipRequest req = new AddInternshipRequest(type, startDate, endDate, studentId, studentFullName, studentMail, studentDepartment, companyName, companyEmail, supervisorName, supervisorMail, supervisorGraduationYear, supervisorGraduationDepartment, supervisorUniversity)
+                Internship internship = new Internship();
+                internship.setType(type);
+                internship.setStartDate(startDate);
+                internship.setEndDate(endDate);
+
+                Student st;
+                Optional<Student> student = studentRepository.findById(studentId);
+                if(student.isPresent()) {
+                    st = student.get();
+                }
+                else {
+                    st = new Student();
+                    st.setId(studentId);
+                    st.setFullName(studentFullName);
+                    st.setMail(studentMail);
+                    st.setRole("Student");
+                    st.setDepartment(studentDepartment);
+                    studentRepository.save(st);
+                }
+                
+                internship.setStudent(st);
+
+                Company company;
+                Optional<Company> optCompany = companyRepository.findByNameAndEmail(companyName, companyEmail);
+                if(optCompany.isPresent()) {
+                    company = optCompany.get();
+                }
+                else {
+                    company = new Company();
+                    //company id: oto
+                    company.setCompanyEmail(companyEmail);
+                    company.setName(companyName);
+                    companyRepository.save(company);
+                }
+
+                internship.setCompany(company);
+
+
+                Supervisor supervisor;
+                Optional<Supervisor> optSupervisor = supervisorRepository.findByNameAndUniversity(supervisorName, supervisorUniversity);
+                if(optSupervisor.isPresent()) {
+                    supervisor = optSupervisor.get();
+                }
+                else {
+                    supervisor = new Supervisor();
+                    //supervisor id: oto
+                    supervisor.setEmail(supervisorMail);
+                    supervisor.setName(supervisorName);
+                    supervisor.setGraduationDepartment(supervisorGraduationDepartment);
+                    supervisor.setGraduationYear(supervisorGraduationYear);
+                    supervisor.setUniversity(supervisorUniversity);
+                    
+                    supervisorRepository.save(supervisor);
+                }
+
+                internship.setSupervisor(supervisor);
+
+                internshipRepository.saveAndFlush(internship);
+
+                list.add(internship);
             }
             
             return list;
