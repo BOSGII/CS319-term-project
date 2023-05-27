d41f2061bbeeedcfd16c64d73f3324c877ce7633
ismail-deniz <ismaildenizcs@gmail.com>
2023-05-27 21:55:11 +0300

    TAController, TAService created and implemented Add and Change Request Objects are created and implemented TA object is updated

---
+++ backend/src/main/java/com/bosgii/internshipmanagement/controllers/TAController.java
@@ -0,0 +1,53 @@
+package com.bosgii.internshipmanagement.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bosgii.internshipmanagement.entities.TA;
+import com.bosgii.internshipmanagement.requests.AddTARequest;
+import com.bosgii.internshipmanagement.requests.ChangeTARequest;
+import com.bosgii.internshipmanagement.services.TAService;
+
+@RestController
+@RequestMapping("/api")
+public class TAController {
+    private final TAService taService;
+
+    public TAController(TAService taService) {
+        this.taService = taService;
+    }
+
+    @GetMapping("/tas")
+    public List<TA> getAllTAs() {
+        return taService.getAllTAs();
+    }
+
+    @PostMapping("/tas")
+	public TA createTA(@RequestBody AddTARequest req) {
+		return taService.createTA(req);
+	}
+
+	@PutMapping("tas/{taId}")
+	public TA changeTADetails(@PathVariable Long taId,
+			@RequestBody ChangeTARequest req) {
+		return taService.changeTADetails(taId, req);
+	}
+
+	@DeleteMapping("/instructors/{instructorId}")
+	public void deleteTA(@PathVariable Long taId) {
+		taService.deleteTA(taId);
+	}
+
+    @PostMapping("/tas/match")
+    public void matchTAs() {
+        taService.matchTAs();
+    }
+}
--- backend/src/main/java/com/bosgii/internshipmanagement/entities/TA.java
+++ backend/src/main/java/com/bosgii/internshipmanagement/entities/TA.java
@@ -1,7 +1,9 @@
 package com.bosgii.internshipmanagement.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="TA")
 public class TA extends Evaluator{
-	
+    public TA(){}
 }
