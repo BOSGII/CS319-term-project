7f5bc64f5318bfa2f34b161177a88fa2af558fb5
İlayda Zehra Yılmaz <zehra.yilmaz@ug.bilkent.edu.tr>
2023-05-27 21:05:52 +0300

    feature f26  styling
    message couldn't be seen before and also changed the message a bit

--- frontend/src/pages/home/SecretaryHomePage.js
+++ frontend/src/pages/home/SecretaryHomePage.js
@@ -2,8 +2,8 @@ import { Container, Typography } from "@mui/material";
 
 export default function SecretaryHomePage() {
   return (
-    <Container>
-      <Typography>Secretary Home Page</Typography>
+    <Container sx={{mt: 15}}>
+      <Typography>Welcome to Secretary Home Page. Please make your choice from the sidebar.</Typography>
     </Container>
   );
 }
