8e0d53a840669f273e58a8bc1c9f0ee89fd7e114
İlayda Zehra Yılmaz <zehra.yilmaz@ug.bilkent.edu.tr>
2023-05-28 00:12:09 +0300

    f29 test

---
+++ frontend/src/pages/admin/AdminPage.js
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { useContext, useEffect, useState } from "react";
+
+export default function AdminPage(){
+    const { user } = useContext(UserContext);
+    const location = useLocation();
+    return(
+        <Container sx={{mt: 15}}>
+            <Typography> Welcome to Admin Page. You can add secretaries here.</Typography>
+        </Container>
+    );
+}
