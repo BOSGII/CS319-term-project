e2704080d1d79470ed20ba1d7bf74f66fa417103
safa54 <82775334+safa54@users.noreply.github.com>
2023-05-28 15:25:14 +0300

    Feature b19 (#34)

--- backend/src/main/java/com/bosgii/internshipmanagement/services/ImportService.java
+++ backend/src/main/java/com/bosgii/internshipmanagement/services/ImportService.java
@@ -26,6 +26,14 @@ import com.bosgii.internshipmanagement.repos.InternshipRepository;
 import com.bosgii.internshipmanagement.repos.StudentRepository;
 import com.bosgii.internshipmanagement.repos.SupervisorRepository;
 //import com.bosgii.internshipmanagement.requests.AddInternshipRequest;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class ImportService {
@@ -45,6 +53,7 @@ public class ImportService {
     }
 
     public List<Internship> importInternshipsFromExcelFile(MultipartFile file) {
+
         List<Internship> list = new ArrayList<Internship>();
         try (Workbook workbook = WorkbookFactory.create(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
@@ -54,90 +63,135 @@ public class ImportService {
                 for (Cell cell : row){
                     rowInfo.add(cell);
                 }
+                
+                try{
+                    String typeString = rowInfo.get(0).getStringCellValue();
+                    
+                    InternshipType type;
+                    if (typeString.equals("CS399"))
+                        type = InternshipType.CS399;
+                    else if (typeString.equals("CS299"))
+                        type = InternshipType.CS299;
+                    else
+                        throw new IllegalArgumentException("Invalid Type");
 
-                String typeString = rowInfo.get(0).getStringCellValue();
-                InternshipType type = InternshipType.CS299;
-                if (typeString.equals("CS399"))
-                    type = InternshipType.CS399;
-                Date startDate = rowInfo.get(1).getDateCellValue();
-                Date endDate = rowInfo.get(2).getDateCellValue();
-                
-                Long studentId = (long) rowInfo.get(3).getNumericCellValue();
-                String studentFullName = rowInfo.get(4).getStringCellValue();
-                String studentMail = rowInfo.get(5).getStringCellValue();
-                String studentDepartment = rowInfo.get(6).getStringCellValue();
-                
-                String companyName = rowInfo.get(7).getStringCellValue();
-                String companyEmail = rowInfo.get(8).getStringCellValue();
-                
-                String supervisorName = rowInfo.get(9).getStringCellValue();
-                String supervisorMail = rowInfo.get(10).getStringCellValue();
-                Date supervisorGraduationYear = rowInfo.get(11).getDateCellValue();
-                String supervisorGraduationDepartment = rowInfo.get(12).getStringCellValue();
-                String supervisorUniversity = rowInfo.get(13).getStringCellValue();
-                
-                //AddInternshipRequest req = new AddInternshipRequest(type, startDate, endDate, studentId, studentFullName, studentMail, studentDepartment, companyName, companyEmail, supervisorName, supervisorMail, supervisorGraduationYear, supervisorGraduationDepartment, supervisorUniversity)
-                Internship internship = new Internship();
-                internship.setType(type);
-                internship.setStartDate(startDate);
-                internship.setEndDate(endDate);
 
-                Student st;
-                Optional<Student> student = studentRepository.findById(studentId);
-                if(student.isPresent()) {
-                    st = student.get();
+                    Date startDate = rowInfo.get(1).getDateCellValue();
+                    Date endDate = rowInfo.get(2).getDateCellValue();
+                    
+                    Long studentId = (long) rowInfo.get(3).getNumericCellValue();
+                    String studentFullName = rowInfo.get(4).getStringCellValue();
+                    String studentMail = rowInfo.get(5).getStringCellValue();
+                    String studentDepartment = rowInfo.get(6).getStringCellValue();
+                    
+                    String companyName = rowInfo.get(7).getStringCellValue();
+                    String companyEmail = rowInfo.get(8).getStringCellValue();
+                    
+                    String supervisorName = rowInfo.get(9).getStringCellValue();
+                    String supervisorMail = rowInfo.get(10).getStringCellValue();
+                    Date supervisorGraduationYear = rowInfo.get(11).getDateCellValue();
+                    String supervisorGraduationDepartment = rowInfo.get(12).getStringCellValue();
+                    String supervisorUniversity = rowInfo.get(13).getStringCellValue();
+
+                   
+                   
+                   
+                    Company company;
+                    Optional<Company> optCompany = companyRepository.findByNameAndEmail(companyName, companyEmail);
+                    if(optCompany.isPresent()) {
+                        company = optCompany.get();
+                    }
+                    else {
+                        company = new Company();
+                        //company id: auto
+                        company.setCompanyEmail(companyEmail);
+                        company.setName(companyName);
+                        //companyRepository.save(company);
+                    }
+
+                    Supervisor supervisor;
+                    Optional<Supervisor> optSupervisor = supervisorRepository.findByNameAndUniversity(supervisorName, supervisorUniversity);
+                    if(optSupervisor.isPresent()) {
+                        supervisor = optSupervisor.get();
+                    }
+                    else {
+                        supervisor = new Supervisor();
+                        //supervisor id: oto
+                        supervisor.setEmail(supervisorMail);
+                        supervisor.setName(supervisorName);
+                        supervisor.setGraduationDepartment(supervisorGraduationDepartment);
+                        supervisor.setGraduationYear(supervisorGraduationYear);
+                        supervisor.setUniversity(supervisorUniversity);
+                        
+                        //supervisorRepository.save(supervisor);
+                    }
+
+                    Student student = new Student();
+                    student.setDepartment(studentDepartment);
+                    student.setMail(studentMail);
+                    student.setRole("Student"); 
+                    student.setFullName(studentFullName);
+                    student.setId(studentId);
+                    
+                    Internship internship = new Internship();
+                    internship.setType(type);
+                    internship.setStartDate(startDate);
+                    internship.setEndDate(endDate);
+                    internship.setCompany(company);
+                    internship.setSupervisor(supervisor);
+                    internship.setStatus(InternshipStatus.WAITING_FOR_COMPANY_APPROVAL);
+                    internship.setStudent(student);
+
+                    list.add(internship);
+
                 }
-                else {
-                    st = new Student();
-                    st.setId(studentId);
-                    st.setFullName(studentFullName);
-                    st.setMail(studentMail);
-                    st.setRole("Student");
-                    st.setDepartment(studentDepartment);
-                    studentRepository.save(st);
+                catch(Exception e){
+
+                    throw new IllegalArgumentException("Problematic line detected on line " +(1 + row.getRowNum() )+ "!" );
+                }
+
+            }
+
+            for (Internship internship: list) {
+                Student student = internship.getStudent();
+                Company company = internship.getCompany();
+                Supervisor supervisor = internship.getSupervisor();
+
+
+                Optional<Student> optStudent = studentRepository.findById(student.getId());
+                if(!optStudent.isPresent()) {
+                
+                    studentRepository.save(student);
+                    sendEmail(student.getMail());
                 }
                 
-                internship.setStudent(st);
-
-                Company company;
-                Optional<Company> optCompany = companyRepository.findByNameAndEmail(companyName, companyEmail);
-                if(optCompany.isPresent()) {
-                    company = optCompany.get();
-                }
-                else {
-                    company = new Company();
-                    //company id: oto
-                    company.setCompanyEmail(companyEmail);
-                    company.setName(companyName);
+                Optional<Company> optCompany = companyRepository.findByNameAndEmail(company.getName(), company.getCompanyEmail());
+                if(!optCompany.isPresent()) {
                     companyRepository.save(company);
                 }
 
-                internship.setCompany(company);
-
-
-                Supervisor supervisor;
-                Optional<Supervisor> optSupervisor = supervisorRepository.findByNameAndUniversity(supervisorName, supervisorUniversity);
-                if(optSupervisor.isPresent()) {
-                    supervisor = optSupervisor.get();
-                }
-                else {
-                    supervisor = new Supervisor();
-                    //supervisor id: oto
-                    supervisor.setEmail(supervisorMail);
-                    supervisor.setName(supervisorName);
-                    supervisor.setGraduationDepartment(supervisorGraduationDepartment);
-                    supervisor.setGraduationYear(supervisorGraduationYear);
-                    supervisor.setUniversity(supervisorUniversity);
-                    
+        
+                Optional<Supervisor> optSupervisor = supervisorRepository.findByNameAndUniversity(supervisor.getName(), supervisor.getEmail());
+                if(!optSupervisor.isPresent()) {
                     supervisorRepository.save(supervisor);
+                } 
+
+
+                Optional<Internship> optInternship = internshipRepository.findByStudentIdAndType(student.getId(), internship.getType());
+
+                if(optInternship.isPresent()){
+                    Internship oldInternship = optInternship.get();
+                    oldInternship.setCompany(company);
+                    oldInternship.setStartDate(internship.getStartDate());
+                    oldInternship.setEndDate(internship.getEndDate());
+                    oldInternship.setSupervisor(supervisor);
+
+                    internshipRepository.save(oldInternship);
+                }
+                else{
+                    internshipRepository.saveAndFlush(internship);
                 }
 
-                internship.setSupervisor(supervisor);
-                internship.setStatus(InternshipStatus.WAITING_FOR_COMPANY_APPROVAL);
-
-                internshipRepository.saveAndFlush(internship);
-
-                list.add(internship);
             }
             
             return list;
@@ -146,5 +200,52 @@ public class ImportService {
         }
         return list;
     }
-    
+
+
+  public static void sendEmail(String recipientEmail) {
+    // Outlook.com configuration
+    String host = "smtp.office365.com";
+    String port = "587";
+    String username = "internshipbilkent@outlook.com";
+    String password = "intern12345";
+
+    // Email content
+    String subject = "Internship Management";
+    String body = "You are registered.";
+
+    try {
+        // Setup mail server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        // Create a session with authentication
+        Session session = Session.getInstance(properties);
+        MimeMessage message = new MimeMessage(session);
+
+        // Set the sender and recipient addresses
+        message.setFrom(new InternetAddress(username));
+        message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail));
+
+        // Set the email subject and body
+        message.setSubject(subject);
+        message.setText(body);
+
+        // Send the email
+        Transport transport = session.getTransport("smtp");
+        transport.connect(host, username, password);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+
+        System.out.println("Email sent successfully!");
+    } catch (MessagingException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+
 }
