7e3e813f6b47e9dfc4a1f9e36f1cfa87ba5fe7c6
Oguz <oguz2101@gmail.com>
2023-05-29 22:06:18 +0300

    styling for secretary upload company evaluation form

--- frontend/src/components/UploadCompanyEvaluationFormButton/UploadCompanyEvaluationFormButton.js
+++ frontend/src/components/UploadCompanyEvaluationFormButton/UploadCompanyEvaluationFormButton.js
@@ -4,7 +4,7 @@ import UploadIcon from "@mui/icons-material/Upload";
 import { useState } from "react";
 import axios from "axios";
 import Upload from "../Upload/Upload";
-
+import UploadFeedback from "../UploadFeedback/UploadFeedback"
 export default function UploadCompanyEvaluationFormButton({
   internshipId,
   setInternshipStatus,
@@ -56,13 +56,15 @@ export default function UploadCompanyEvaluationFormButton({
           setOpen(false);
         }}
       >
-        <Typography>Enter supervisor grade:</Typography>
+        <Typography variant="h5" style={{ fontSize: "24px" }}>
+          Enter supervisor grade:
+        </Typography>
         <input
           type="number"
           value={supervisorGrade}
           onChange={(el) => setSupervisorGrade(el.target.value)}
         ></input>
-        <Upload
+        <UploadFeedback
           acceptedFileTypes={["PDF"]}
           handleSubmit={handleSubmit}
           handleCancel={handleCancel}
