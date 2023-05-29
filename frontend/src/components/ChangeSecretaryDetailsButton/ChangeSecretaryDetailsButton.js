2d703b923bcf5b2ca8ffc4fe11e1943409264ac6
İlayda Zehra Yılmaz <85749680+izy-glitch@users.noreply.github.com>
2023-05-29 20:13:17 +0300

    Feature f29 admin page (#52)

---
+++ frontend/src/components/ChangeSecretaryDetailsButton/ChangeSecretaryDetailsButton.js
@@ -0,0 +1,5 @@
+import {IconButton} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+export default function ChangeSecretaryDetailsButton(){
+    return <IconButton><EditIcon></EditIcon></IconButton>;
+}
---
+++ frontend/src/components/Secretary/Secretary.js
@@ -0,0 +1,52 @@
+import {
+    Button, 
+    ListItem, 
+    ListItemText,
+    Stack, 
+    Box, 
+    Card } from "@mui/material";
+import { 
+    useState, 
+    useEffect } from "react";
+import ChangeSecretaryDetailsButton from "../ChangeSecretaryDetailsButton/ChangeSecretaryDetailsButton";
+import DeleteSecretaryButton from "../DeleteSecretaryButton/DeleteSecretaryButton";
+
+//Secretary in Secretary List to be shown on the Admin Page
+export default function Secretary({secretary, refreshSecretaries}){
+
+    const[fullName, setFullName] = useState( secretary.fullName );
+    const[mail, setMail] = useState( secretary.mail );
+
+    useEffect( () => {
+        setFullName(secretary.fullName);
+        setMail(secretary.mail);
+    }, [secretary]);
+
+    return( 
+        <Card elevation={10} style={{ borderRadius: 15 }}>
+            <ListItem >
+                <Stack spacing={70} direction='row'>
+                    <ListItemText>
+                        <Stack spacing={40} direction='row'>
+                            <Box sx={{maxWidth: 30}}>{fullName}</Box><Box sx={{maxWidth: 30}}>{mail}</Box>
+                        </Stack>
+                    </ListItemText>
+                <Stack spacing={5}direction='row'>
+                    <Box>
+                        <ChangeSecretaryDetailsButton/>
+                    </Box>
+                    <Box>
+                        <DeleteSecretaryButton
+                        secretaryId = {secretary.id}
+                        refreshSecretaries = {refreshSecretaries}
+                        />
+                    </Box>
+                </Stack>
+            </Stack>
+        
+        </ListItem>
+    </Card>
+        
+    );
+
+}
