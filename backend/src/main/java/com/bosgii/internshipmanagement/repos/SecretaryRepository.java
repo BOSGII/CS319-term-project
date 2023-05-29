2c165bdfc12c2ea29ed16066c51bb3ce3eca154d
Goktug <goktugyilmaz01@gmail.com>
2023-05-29 12:37:08 +0300

    create AdminController/Service, SecretaryService

---
+++ backend/src/main/java/com/bosgii/internshipmanagement/repos/SecretaryRepository.java
@@ -0,0 +1,8 @@
+package com.bosgii.internshipmanagement.repos;
+
+import com.bosgii.internshipmanagement.entities.Secretary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecretaryRepository extends JpaRepository<Secretary, Long> {
+
+}
---
+++ backend/src/main/java/com/bosgii/internshipmanagement/services/AdminService.java
@@ -0,0 +1,28 @@
+package com.bosgii.internshipmanagement.services;
+
+import com.bosgii.internshipmanagement.entities.Secretary;
+import com.bosgii.internshipmanagement.requests.AddSecretaryRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+    private SecretaryService secretaryService;
+
+    public AdminService(SecretaryService secretaryService){
+        this.secretaryService = secretaryService;
+    }
+
+    public List<Secretary> getAllSecretaries(){
+        return secretaryService.getAllSecretaries();
+    }
+
+    public Secretary createSecretary(AddSecretaryRequest req){
+        return secretaryService.createSecretary(req);
+    }
+
+    public void deleteSecretary(Long id) {
+        secretaryService.deleteSecretary(id);
+    }
+}
